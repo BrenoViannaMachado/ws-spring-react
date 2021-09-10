4bb4dd8f02642cd6dc8366b5b606fa6df7e616d5
BrenoViannaMachado <brenovianna@hotmail.com>
2021-09-10 19:46:41 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
+    <>
     <div>
+      <NavBar />
       <h1 className="text-primary">Olá mundo!</h1>
     </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,10 @@
+function NavBar() {
+    return (
+      <div>
+        <h2 className="text-primary">Olá mundo!</h2>
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  
