62fd2900fd5e799a6c0615f2f84d24727c8e63d5
BrenoViannaMachado <brenovianna@hotmail.com>
2021-09-12 19:32:58 -0300

    credits

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ function Footer() {
     <p className="text-light">App desenvolvido por <a href="https://github.com/BrenoViannaMachado" target="_blank" rel="noreferrer">Breno Vianna</a></p>
     <p className="text-light"><small><strong>Semana Spring React</strong><br/>
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/breno_vianna_" target="_blank" rel="noreferrer">@breno_vianna_</a></small></p>
+    <p className="text-light">Créditos: <a href="https://devsuperior.com.br/" target="_blank" rel="noreferrer">DevSuperior</a></p>
   </div>
 </footer>
     );
