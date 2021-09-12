79819323485b8bcc1a02e6bd410bcb91766b5e90
BrenoViannaMachado <brenovianna@hotmail.com>
2021-09-12 18:53:24 -0300

    footer att

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ function Footer() {
     return (
       <footer className="footer mt-auto py-3 bg-dark">
   <div className="container">
-    <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Breno Vianna</a></p>
+    <p className="text-light">App desenvolvido por <a href="https://github.com/BrenoViannaMachado" target="_blank" rel="noreferrer">Breno Vianna</a></p>
     <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@breno_vianna_</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/breno_vianna_" target="_blank" rel="noreferrer">@breno_vianna_</a></small></p>
   </div>
 </footer>
     );
